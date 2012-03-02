e473e0f68bfd4f479c99946d45cb26c99b23bdb7
James Treworgy <jamietre@gmail.com>
2012-03-02 08:42:00 -0500

    Fix for can't disable fill effect using altImage

--- dist/jquery.imagemapster.js
+++ dist/jquery.imagemapster.js
@@ -1032,16 +1032,17 @@ A jQuery plugin to enhance image maps.
 
                 $.each(me.shapes, function (i,s) {
                     shapeContext.save();
-                    if (s.options.alt_image) {
-                        me.addAltImage(shapeContext, s.options.alt_image, s.mapArea, s.options);
-                    } else if (s.options.fill) {
-
-                        shapeContext.beginPath();
-                        me.renderShape(shapeContext, s.mapArea);
-                        shapeContext.closePath();
-                        //shapeContext.clip();
-                        shapeContext.fillStyle = me.css3color(s.options.fillColor, s.options.fillOpacity);
-                        shapeContext.fill();
+                    if (s.options.fill) {
+                        if (s.options.alt_image) {
+                            me.addAltImage(shapeContext, s.options.alt_image, s.mapArea, s.options);
+                        } else {
+                            shapeContext.beginPath();
+                            me.renderShape(shapeContext, s.mapArea);
+                            shapeContext.closePath();
+                            //shapeContext.clip();
+                            shapeContext.fillStyle = me.css3color(s.options.fillColor, s.options.fillOpacity);
+                            shapeContext.fill();
+                        }
                     }
                     shapeContext.restore();
                 });
