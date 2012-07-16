27a3f5d4bc6f1572ffa19f3b2499d7cd48d6238f
James Treworgy <jamietre@gmail.com>
2012-07-16 08:30:44 -0400

    Fix tooltips

--- src/mapdata.js
+++ src/mapdata.js
@@ -96,7 +96,7 @@
                 if (me.options.showToolTip) {
                     $.each(arData,function(i,e) {
                         if (e.effectiveOptions().toolTip) {
-                            e.showToolTip();
+                            e.showTooltip();
                         }
                     });
                 }
@@ -154,7 +154,7 @@
             me.ensureNoHighlight();
 
             if (opts.toolTipClose && $.inArray('area-mouseout', opts.toolTipClose) >= 0 && me.activeToolTip) {
-                me.clearToolTip();
+                me.clearTooltip();
             }
         };
         this.click = function (e) {
@@ -764,7 +764,7 @@
         me.images.clear();
 
         this.image = null;
-        u.ifFunction(this.clearToolTip, this);
+        u.ifFunction(this.clearTooltip, this);
     };
     // Compelete cleanup process for deslecting items. Called after a batch operation, or by AreaData for single
     // operations not flagged as "partial"
