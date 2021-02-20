12de758c4c1957293093a37d94d159e70e24fe85
techfg <barry@techfg.com>
2021-02-20 01:10:39 -0800

    Resolves #381 - tests should wait for onConfigured

--- tests/resize.tests.js
+++ tests/resize.tests.js
@@ -9,6 +9,7 @@ this.tests.push(
       'use strict';
 
       var img = $('img'),
+        me = this,
         altImages = {
           img1: 'images/usa_map_720_alt_1.jpg',
           img2: 'images/usa_map_720_alt_2.jpg',
@@ -16,64 +17,72 @@ this.tests.push(
           img4: 'images/usa_map_720_alt_4.jpg',
           img5: 'images/usa_map_720_alt_5.jpg'
         },
+        map,
+        x,
+        y,
+        newWidth;
+
+      me.when(function (cb) {
         map = img.mapster({
-          altImages: altImages
-        }),
-        x = parseInt(img.css('width').replace('px', ''), 10),
-        y = parseInt(img.css('height').replace('px', ''), 10),
-        newWidth = x + 200;
-
-      this.when(function (cb) {
-        map.mapster('resize', newWidth, 0, cb);
+          altImages: altImages,
+          onConfigured: cb
+        });
       }).then(function () {
-        var expectedWidth = newWidth + 'px',
-          expectedHeight = Math.round((newWidth / x) * y) + 'px';
-        a.equals(
-          img.css('width'),
-          expectedWidth,
-          'image width is correct after resize'
-        );
-        a.equals(
-          img.css('height'),
-          expectedHeight,
-          'image height is correct after resize'
-        );
-
-        var wrapper = img.closest('div');
-
-        a.equals(
-          wrapper.attr('id').substring(0, 12),
-          'mapster_wrap',
-          'sanity check - we have the wrapper element'
-        );
-        a.equals(
-          wrapper.css('width'),
-          expectedWidth,
-          'wrapper width matches image width'
-        );
-        a.equals(
-          wrapper.css('height'),
-          expectedHeight,
-          'wrapper height matches image height'
-        );
-        Object.values(altImages).map(function (ai) {
-          var selector = 'img[src="' + ai + '"]',
-            altImg = $(wrapper).children(selector);
+        x = parseInt(img.css('width').replace('px', ''), 10);
+        y = parseInt(img.css('height').replace('px', ''), 10);
+        newWidth = x + 200;
+        me.when(function (cb) {
+          map.mapster('resize', newWidth, 0, cb);
+        }).then(function () {
+          var expectedWidth = newWidth + 'px',
+            expectedHeight = Math.round((newWidth / x) * y) + 'px';
           a.equals(
-            altImg.css('width'),
+            img.css('width'),
             expectedWidth,
-            'altimage ' + ai + ' width matches image width'
+            'image width is correct after resize'
           );
           a.equals(
-            altImg.css('height'),
+            img.css('height'),
             expectedHeight,
-            'altimage ' + ai + ' height matches image height'
+            'image height is correct after resize'
+          );
+
+          var wrapper = img.closest('div');
+
+          a.equals(
+            wrapper.attr('id').substring(0, 12),
+            'mapster_wrap',
+            'sanity check - we have the wrapper element'
           );
           a.equals(
-            altImg.css('display'),
-            'none',
-            'altimage ' + ai + ' has display of none'
+            wrapper.css('width'),
+            expectedWidth,
+            'wrapper width matches image width'
           );
+          a.equals(
+            wrapper.css('height'),
+            expectedHeight,
+            'wrapper height matches image height'
+          );
+          Object.values(altImages).map(function (ai) {
+            var selector = 'img[src="' + ai + '"]',
+              altImg = $(wrapper).children(selector);
+            a.equals(
+              altImg.css('width'),
+              expectedWidth,
+              'altimage ' + ai + ' width matches image width'
+            );
+            a.equals(
+              altImg.css('height'),
+              expectedHeight,
+              'altimage ' + ai + ' height matches image height'
+            );
+            a.equals(
+              altImg.css('display'),
+              'none',
+              'altimage ' + ai + ' has display of none'
+            );
+          });
         });
       });
     })
