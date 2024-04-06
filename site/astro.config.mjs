50679c28ab6065fffe69b83e6e2fe98fc08fe4b0
techfg <barry@techfg.com>
2024-04-06 03:20:18 -0700

    chore: update astro config with site url & base

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://techfg.github.io',
+  base: 'ImageMapster',
   integrations: [
     starlight({
       title: 'My Docs',
